7c1115c35eebc744c331d8d026fb62344db671c3
alanabello <ala.abello@duocuc.cl>
2026-07-18 01:16:01 -0400

    service modificado	modified:   service-worker.js modificado	modificed:   www/index.html modificado	modified:   www/service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ruta3-v5';
+const CACHE_NAME = 'ruta3-v6';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
