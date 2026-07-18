d7220e857a12bb3ac399666eea5528df390fcb5a
alanabello <ala.abello@duocuc.cl>
2026-07-18 00:50:21 -0400

    error de apk error de apk#	modified:   www/service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ruta3-v4';
+const CACHE_NAME = 'ruta3-v5';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
